43585cc38dd7cb4c6f2f0bc6d351660b0ba76285
AD <dungnknd97@gmail.com>
2023-01-19 17:55:06 +0700

    update vue config js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  productionSourceMap: false,
+  publicPath: "/",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/crypto-dashboard-3/" // Thay tên repository
+      : "/",
+});
